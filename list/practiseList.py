841db2ee08d05ee29dadb7a7414127b8509a7861
Mayun-binod <shresthabinod334@gmail.com>
2024-09-21 17:28:07 +0545

    added

---
+++ list/practiseList.py
@@ -0,0 +1,44 @@
+# write a program to store 3 fruits in a list enter by the user
+# list=[] #empty list 
+# listAdd=input("enter the fruits:")
+# list.append(listAdd)
+# listAdd2=input("enter the fruits:")
+# list.append(listAdd2)
+# listAdd3=input("enter the fruits:")
+# list.append(listAdd3)
+# print(list)
+# another method
+# lists=[]
+# for i in range(0,4):
+#     listsAd=input(f"enter the fruits {i+1}:")
+#     lists.append(listsAd)
+#     print(lists)
+
+
+# write a program to accepts marks of 6 students and display them in a sorted manner.
+
+# studentsMark=[]
+# accepts=int(input("enter the students marks:"))
+# studentsMark.append(accepts)
+# accepts2=int(input("enter the students marks:"))
+# studentsMark.append(accepts2)
+# accepts3=int(input("enter the students marks:"))
+# studentsMark.append(accepts3)
+# accepts44=int(input("enter the students marks:"))
+# studentsMark.append(accepts44)
+# accepts4=int(input("enter the students marks:"))
+# studentsMark.append(accepts4)
+# accepts5=int(input("enter the students marks:"))
+# studentsMark.append(accepts5)
+# accepts6=int(input("enter the students marks:"))
+# studentsMark.append(accepts6)
+# studentsMark.sort()
+# print(studentsMark)
+# another method
+# studentsMark=[]
+# for i in range(6):
+#     accepts=int(input(f"enter the students marks {i+1}: "))
+#     studentsMark.append(accepts)
+#     studentsMark.sort()
+#     print(studentsMark)
+
